368a61ab4836aef04d5d942a07fe25b18de336dd
Kei Ito <kei.itof@gmail.com>
2020-08-30 14:05:00 +0900

    feat: sort exports

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
+ */
 const g = globalThis;
-export {g as globalThis};
 export const {Object} = g;
-export const {Array} = g;
-export const {ArrayBuffer} = g;
-export const {Atomics} = g;
-export const {BigInt} = g;
-export const {BigInt64Array} = g;
-export const {BigUint64Array} = g;
+export const {Function} = g;
 export const {Boolean} = g;
-export const {DataView} = g;
-export const {Date} = g;
+export const {Symbol} = g;
+
+export const {Infinity} = g;
+export const {NaN} = g;
+export const {undefined} = g;
+export {g as globalThis};
+
 // export const {Error} = g;
 export class Error extends g.Error {
 
@@ -24,55 +26,68 @@ export class Error extends g.Error {
     }
 }
 export const {EvalError} = g;
-export const {Float32Array} = g;
-export const {Float64Array} = g;
-export const {Function} = g;
-export const {Infinity} = g;
-export const {Int16Array} = g;
-export const {Int32Array} = g;
-export const {Int8Array} = g;
-export const {Intl} = g;
-export const {JSON} = g;
-export const {Map} = g;
-export const {Math} = g;
-export const {NaN} = g;
-export const {Number} = g;
-export const {Promise} = g;
-export const {Proxy} = g;
 export const {RangeError} = g;
 export const {ReferenceError} = g;
-export const {Reflect} = g;
-export const {RegExp} = g;
-export const {Set} = g;
-export const {SharedArrayBuffer} = g;
-export const {String} = g;
-export const {Symbol} = g;
 export const {SyntaxError} = g;
 export const {TypeError} = g;
 export const {URIError} = g;
-export const {URL} = g;
-export const {URLSearchParams} = g;
-export const {Uint16Array} = g;
-export const {Uint32Array} = g;
-export const {Uint8Array} = g;
-export const {Uint8ClampedArray} = g;
-export const {WeakMap} = g;
-export const {WeakSet} = g;
-export const {WebAssembly} = g;
-export const {clearInterval} = g;
-export const {clearTimeout} = g;
-export const {console} = g;
-export const {decodeURI} = g;
-export const {decodeURIComponent} = g;
-export const {encodeURI} = g;
-export const {encodeURIComponent} = g;
-export const {escape} = g;
+
+export const {Number} = g;
+export const {BigInt} = g;
+export const {Math} = g;
+export const {Date} = g;
+
 // export const {eval} = g;
 export const {isFinite} = Number;
 export const {isNaN} = Number;
 export const {parseFloat} = Number;
 export const {parseInt} = Number;
+export const {encodeURI} = g;
+export const {encodeURIComponent} = g;
+export const {decodeURI} = g;
+export const {decodeURIComponent} = g;
+// export const {escape} = g;
+// export const {unescape} = g;
+
+export const {String} = g;
+export const {RegExp} = g;
+
+export const {Array} = g;
+export const {Int8Array} = g;
+export const {Uint8Array} = g;
+export const {Uint8ClampedArray} = g;
+export const {Int16Array} = g;
+export const {Uint16Array} = g;
+export const {Int32Array} = g;
+export const {Uint32Array} = g;
+export const {Float32Array} = g;
+export const {Float64Array} = g;
+export const {BigInt64Array} = g;
+export const {BigUint64Array} = g;
+
+export const {Map} = g;
+export const {Set} = g;
+export const {WeakMap} = g;
+export const {WeakSet} = g;
+
+export const {ArrayBuffer} = g;
+export const {SharedArrayBuffer} = g;
+export const {Atomics} = g;
+export const {DataView} = g;
+export const {JSON} = g;
+
+export const {Reflect} = g;
+export const {Proxy} = g;
+
+export const {Intl} = g;
+
+export const {WebAssembly} = g;
+
+export const {Promise} = g;
+export const {URL} = g;
+export const {URLSearchParams} = g;
+export const {clearInterval} = g;
+export const {clearTimeout} = g;
+export const {console} = g;
 export const {setInterval} = g;
 export const {setTimeout} = g;
-export const {undefined} = g;
-export const {unescape} = g;
