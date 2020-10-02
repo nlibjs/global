fdf1af15fed9500d6041963014a44ad81deb4b41
Kei Ito <kei.itof@gmail.com>
2020-10-03 02:35:26 +0900

    refactor: add createLineFragmentReader

--- src/readLine.ts
+++ src/readLine.ts
@@ -1,3 +1,5 @@
+import {Object} from './global';
+
 export const LF = 0x0A;
 export const CR = 0x0D;
 
@@ -20,58 +22,62 @@ export const isCRLF = (
     index: number,
 ) => source.charCodeAt(index) === CR && source.charCodeAt(index + 1) === LF;
 
+export interface LineFragmentReader<T> {
+    readonly flush: () => Generator<string, void, void>,
+    (chunk: T): Generator<string, void, void>,
+}
+
+export const createLineFragmentReader = <T>(): LineFragmentReader<T> => {
+    let remainder = '';
+    let maybeCRLF = false;
+    return Object.defineProperty(
+        function* (
+            fragment: T,
+        ): Generator<string, void, void> {
+            let string = `${remainder}${fragment}`;
+            if (maybeCRLF && string.charCodeAt(0) === LF) {
+                string = string.slice(1);
+            }
+            let lineStart = 0;
+            const {length} = string;
+            while (lineStart < length) {
+                const lineEnd = findCodePointIndex(string, lineStart, CR, LF);
+                if (lineEnd < 0) {
+                    break;
+                }
+                yield string.slice(lineStart, lineEnd);
+                lineStart = lineEnd + (isCRLF(string, lineEnd) ? 2 : 1);
+            }
+            remainder = string.slice(lineStart);
+            maybeCRLF = string.charCodeAt(length - 1) === CR;
+        },
+        'flush',
+        {
+            *value() {
+                if (remainder) {
+                    yield remainder;
+                }
+            },
+        },
+    ) as LineFragmentReader<T>;
+};
+
 export const readLine = function* <T>(
     source: Iterable<T>,
 ): Generator<string, void, void> {
-    let remainder = '';
-    let maybeCRLF = false;
+    const read = createLineFragmentReader();
     for (const chunk of typeof source === 'string' ? [source] : source) {
-        let string = `${remainder}${chunk}`;
-        if (maybeCRLF && string.charCodeAt(0) === LF) {
-            string = string.slice(1);
-        }
-        let lineStart = 0;
-        const {length} = string;
-        while (lineStart < length) {
-            const lineEnd = findCodePointIndex(string, lineStart, CR, LF);
-            if (lineEnd < 0) {
-                break;
-            }
-            yield string.slice(lineStart, lineEnd);
-            lineStart = lineEnd + (isCRLF(string, lineEnd) ? 2 : 1);
-        }
-        remainder = string.slice(lineStart);
-        maybeCRLF = string.charCodeAt(length - 1) === CR;
+        yield* read(chunk);
     }
-    if (remainder) {
-        yield remainder;
-    }
+    yield* read.flush();
 };
 
 export const readLineAsync = async function* <T>(
     source: AsyncIterable<T>,
 ): AsyncGenerator<string, void, void> {
-    let remainder = '';
-    let maybeCRLF = false;
+    const read = createLineFragmentReader();
     for await (const chunk of source) {
-        let string = `${remainder}${chunk}`;
-        if (maybeCRLF && string.charCodeAt(0) === LF) {
-            string = string.slice(1);
-        }
-        let lineStart = 0;
-        const {length} = string;
-        while (lineStart < length) {
-            const lineEnd = findCodePointIndex(string, lineStart, CR, LF);
-            if (lineEnd < 0) {
-                break;
-            }
-            yield string.slice(lineStart, lineEnd);
-            lineStart = lineEnd + (isCRLF(string, lineEnd) ? 2 : 1);
-        }
-        remainder = string.slice(lineStart);
-        maybeCRLF = string.charCodeAt(length - 1) === CR;
+        yield* read(chunk);
     }
-    if (remainder) {
-        yield remainder;
-    }
+    yield* read.flush();
 };
